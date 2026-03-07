af959ba109db0ecc69db758d8cbcf3ca3384891e
cpascarlos <charleman08@gmail.com>
2026-03-07 14:13:43 +0100

    Retrait du % dans la variable total pour rrdtool

--- sonde_disque.sh
+++ sonde_disque.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 date=$(date '+%Y-%m-%d %H:%M:%S')
-total=$(df -h / | awk 'NR==2 {print $5}')
-echo "[$date] [DISQUE] Utilisation Globale: $total"
+total=$(df -h / | awk 'NR==2 {print $5}' | tr -d '%')
+echo "[$date] [DISQUE] Utilisation Globale: $total%"
