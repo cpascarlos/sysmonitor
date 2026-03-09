1a7633d7ebb1a4c22f9bcbf759a6dc7e05f6e716
cpascarlos <charleman08@gmail.com>
2026-03-09 18:50:21 +0100

    J'ai oublié le $ avant rrd

--- export_json.sh
+++ export_json.sh
@@ -4,7 +4,7 @@ rrd=""
 export_file=""
 
 /usr/bin/rrdtool xport --json --start -60m --step 60 \
-    DEF:c=rrd:cpu:AVERAGE \
-    DEF:r=rrd:ram:AVERAGE \
-    DEF:d=rrd:disque:AVERAGE \
+    DEF:c=$rrd:cpu:AVERAGE \
+    DEF:r=$rrd:ram:AVERAGE \
+    DEF:d=$rrd:disque:AVERAGE \
     XPORT:c:"CPU" XPORT:r:"RAM" XPORT:d:"DISQUE" > $export_file
