4b2e785a9d91965a212ff57dcb86ce060a207b09
cpascarlos <charleman08@gmail.com>
2026-03-16 09:11:52 +0100

    remplacement de disk par disque

--- create_rrd.sh
+++ create_rrd.sh
@@ -3,5 +3,5 @@
 /usr/bin/rrdtool create monitor.rrd --step 60 \
   DS:cpu:GAUGE:600:0:100 \
   DS:ram:GAUGE:600:0:100 \
-  DS:disk:GAUGE:600:0:100 \
+  DS:disque:GAUGE:600:0:100 \
   RRA:AVERAGE:0.5:1:43200
